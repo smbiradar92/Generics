8f3cb49666a032806ce8d4880c3dde8e799682cb
smbiradar92 <smbiradar92@gmail.com>
2022-03-12 00:44:58 +0530

    Adding Generic method Array list

--- src/com/printarray/PrintArray.java
+++ src/com/printarray/PrintArray.java
@@ -2,31 +2,16 @@ package com.printarray;
 
 public class PrintArray {
 
-	public static void toPrint(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");
+	public static <E> void toPrint(E[] arr) { 				//E, Array list is a generic class
+		for (E e : arr) {
+			System.out.printf(e + " ");
 		}
-		System.out.println("\t");
+		System.out.println("\n");
 	}
-
-	public static void toPrint(double[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");
-		}
-		System.out.println("\t");
-	}
-
-	public static void toPrint(String[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");
-		}
-		System.out.println("\t");
-	}
-
 	public static void main(String[] args) {
-		int[] intArr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		Integer[] intArr = new Integer[] { 1, 2, 3, 4, 5, 6, 7 };   // only non primitive data types to be passed
 		toPrint(intArr);
-		double[] doubleArr = new double[] { 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1 };
+		Double[] doubleArr = new Double[] { 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1 };
 		toPrint(doubleArr);
 		String[] StringArr = new String[] { "Hello,", "How", "Are", "you" };
 		toPrint(StringArr);
