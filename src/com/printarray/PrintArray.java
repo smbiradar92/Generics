509af7981cdcff51fd2cd2690fcdf9bf72692975
smbiradar92 <smbiradar92@gmail.com>
2022-03-12 12:17:22 +0530

    Print Array by Generic method

--- src/com/printarray/PrintArray.java
+++ src/com/printarray/PrintArray.java
@@ -1,20 +1,23 @@
 package com.printarray;
 
-public class PrintArray {
-
-	public static <E> void toPrint(E[] arr) { 				//E, Array list is a generic class
-		for (E e : arr) {
-			System.out.printf(e + " ");
+public class PrintArray <T> {
+	
+	public void toPrint(T[] arr) { 				//E, Array list is a generic class
+		for (T t : arr) {
+			System.out.printf(t + " ");
 		}
 		System.out.println("\n");
 	}
 	public static void main(String[] args) {
 		Integer[] intArr = new Integer[] { 1, 2, 3, 4, 5, 6, 7 };   // only non primitive data types to be passed
-		toPrint(intArr);
+		PrintArray<Integer> intPrintArr = new PrintArray<Integer>();     
+		intPrintArr.toPrint(intArr);
 		Double[] doubleArr = new Double[] { 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1 };
-		toPrint(doubleArr);
+		PrintArray<Double> doublePrintArr = new PrintArray<>();     
+		doublePrintArr.toPrint(doubleArr);
 		String[] StringArr = new String[] { "Hello,", "How", "Are", "you" };
-		toPrint(StringArr);
+		PrintArray<String> stringPrintArr = new PrintArray<>();
+		stringPrintArr.toPrint(StringArr);
 	}
 
 }
